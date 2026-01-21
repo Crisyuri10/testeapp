db6645f4caad513e663c9bc4f353b7d045e22fdc
Crisyuri10 <crisyurisantos10@gmail.com>
2026-01-21 16:33:43 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -94,18 +94,3 @@ if enviar:
             st.success("✅ Venda Efetuada")
         else:
             st.error("❌ Erro ao salvar no banco")
-
-        
- 
-
-st.subheader("Tabela")
-
-# Lê os dados da tabela Vendas
-dados = requests.get(f"{link}/Vendas.json", verify=False).json()
-
-# Se não tiver nada no banco
-if not dados:
-    st.info("Nenhuma venda cadastrada")
-else:
-    # Converte os dados em tabela
-    st.dataframe(pd.DataFrame.from_dict(dados, orient="index"))
